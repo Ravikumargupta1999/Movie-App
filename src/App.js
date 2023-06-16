ec988466eef6bafec080054f27f8b599a8df132a
unknown <ravikumargupta391999@gmail.com>
2023-06-16 23:02:32 +0530

    Pros In functional Component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import Student from "./Student";
 function App() {
   return (
     <>
-       <Student name ="Alexa" marks = {80}/>
-       <Student name ="Siri" marks = {70}/>
-       <Student name ="AlA" marks = {60}/>
+       <Student stuname ="Alexa" marks = {80}/>
+       <Student stuname ="Siri" marks = {70}/>
+       <Student stuname ="RISHIKA" marks = {60}/>
+       <Student stuname ="RAVI" />
        <Student  marks = {60}/>
     </>
   );
-}
+  }
+
+  Student.defaultProps ={
+    stuname : "Student",
+    marks : "N.A."
+  }
 
 export default App;
