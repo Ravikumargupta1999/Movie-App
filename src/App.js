bdd1d6810cc09476a92bc9252002f1360ea87d5a
unknown <ravikumargupta391999@gmail.com>
2023-06-16 23:07:41 +0530

    Pros In functional Component

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import Student from "./Student";
 function App() {
   return (
     <>
-       <Student stuname ="Alexa" marks = {80}/>
-       <Student stuname ="Siri" marks = {70}/>
-       <Student stuname ="RISHIKA" marks = {60}/>
-       <Student stuname ="RAVI" />
-       <Student  marks = {60}/>
+      <Student stuname="Alexa" marks={80} />
+      <Student stuname="Siri" marks={70} />
+      <Student stuname="RISHIKA" marks={60} />
+      <Student stuname="RAVI" />
+      <Student marks={60} />
     </>
   );
-  }
+}
 
-  Student.defaultProps ={
-    stuname : "Student",
-    marks : "N.A."
-  }
+Student.defaultProps = {
+  stuname: "Student",
+  marks: "N.A."
+}
 
 export default App;
--- src/Student.js
+++ src/Student.js
@@ -1,16 +1,30 @@
-import React  from "react";
+// import React  from "react";
 
-class Student extends React.Component{
-    render(){
-        console.log(this.props);
-        const {stuname,marks} = this.props;
-        return(
-            <>
-               <h1>Hello, {stuname}</h1>
-               <p>You have secured {marks} marks</p>
-            </>
-        )
-    }
+// class Student extends React.Component{
+//     render(){
+//         console.log(this.props);
+//         const {stuname,marks} = this.props;
+//         return(
+// <>
+//    <h1>Hello, {stuname}</h1>
+//    <p>You have secured {marks} marks</p>
+//     <hr/>
+// </>
+//         )
+//     }
+// }
+
+
+function Student(props) {
+    const {stuname,marks} = props;
+    // props.stuname = "Ravi";  // props can't be modified
+    return (
+        <>
+            <h1>Hello, {stuname}</h1>
+            <p>You have secured {marks} marks</p>
+            <hr />
+        </>
+    )
 }
 
 export default Student;
