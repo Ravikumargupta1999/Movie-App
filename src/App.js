8977f1246f849a0df22c71b7b156c723d5f43123
unknown <ravikumargupta391999@gmail.com>
2023-06-17 13:47:26 +0530

    Lifting state from movieList to APP

--- src/App.js
+++ src/App.js
@@ -1,16 +1,84 @@
-import HeadingOne from "./HeadingOne";
-import HeadingTwo from "./HeadingTwo";
+import React from "react";
 import MovieList from "./MovieList";
 import Navbar from "./Navbar";
+import { movies } from "./moviesdata";
 
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+        movies: movies,
+        cartCount : 0
+    
+    }
+    // this.addStars = this.addStars.bind(this);
+}
 
-function App() {
-  return (
-    <>
-       <Navbar/>
-       <MovieList/>
-    </>
-  );
+handleIncStars = (movie) => {
+    const { movies } = this.state;
+    const mid = movies.indexOf(movie);
+
+    if (movies[mid].stars >= 5) {
+        return;
+    }
+    movies[mid].stars += 0.5
+
+    this.setState({
+        movies
+    });
+}
+
+handleDecStars = (movie) => {
+    const { movies } = this.state;
+    const mid = movies.indexOf(movie);
+
+    if (movies[mid].stars <= 0) {
+        return;
+    }
+    movies[mid].stars -= 0.5;
+
+    this.setState({
+        movies :movies
+    })
+
+}
+
+handleToggleFav = (movie) => {
+    const { movies } = this.state;
+    const mid = movies.indexOf(movie);
+
+    movies[mid].fav = !movies[mid].fav;
+
+    this.setState({
+        movies
+    })
+}
+
+handleToggleCart = (movie) => {
+    const { movies } = this.state;
+    const mid = movies.indexOf(movie);
+
+    movies[mid].isInCart = !movies[mid].isInCart;
+
+    this.setState({
+        movies
+    })
+}
+
+  render() {
+    const {movies} = this.state;
+    return (
+      <>
+        <Navbar />
+        <MovieList movies = {movies}
+                   addStars={this.handleIncStars}
+                   decStars={this.handleDecStars}
+                   toggleFav={this.handleToggleFav}
+                   toggleCart={this.handleToggleCart} 
+                   />
+      </>
+    );
+  }
 }
 
 export default App;
--- src/MovieList.js
+++ src/MovieList.js
@@ -1,122 +1,22 @@
-import React from "react";
+import {Component} from "react";
 import MovieCard from "./Moviecard";
 
-class MovieList extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            movies: [
-
-                {
-                    id : 1,
-                    title: 'The Avengers',
-                    plot:
-                        "Earth's mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.",
-                    poster:
-                        'https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg',
-                    rating: '8.0',
-                    price: 99,
-                    stars: 0,
-                    fav: false,
-                    isInCart: false
-                },
-                {
-                    id : 2,
-                    title: 'Iron Man',
-                    plot:
-                        'After being held captive in an Afghan cave, billionaire engineer Tony starsk creates a unique weaponized suit of armor to fight evil.',
-                    poster:
-                        'https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg',
-                    rating: '7.9',
-                    price: 139,
-                    stars: 0,
-                    fav: false,
-                    isInCart: false
-                },
-                {
-                    id : 3,
-                    title: 'The Dark Knight',
-                    plot:
-                        'When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.',
-                    poster:
-                        'https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX300.jpg',
-                    rating: '9.0',
-                    price: 199,
-                    stars: 0,
-                    fav: false,
-                    isInCart: false
-                }
-            
-            ]
-        
-        }
-        // this.addStars = this.addStars.bind(this);
-    }
-
-    handleIncStars = (movie) => {
-        const { movies } = this.state;
-        const mid = movies.indexOf(movie);
-
-        if (movies[mid].stars >= 5) {
-            return;
-        }
-        movies[mid].stars += 0.5
-
-        this.setState({
-            movies
-        });
-    }
-
-    handleDecStars = (movie) => {
-        const { movies } = this.state;
-        const mid = movies.indexOf(movie);
-
-        if (movies[mid].stars <= 0) {
-            return;
-        }
-        movies[mid].stars -= 0.5;
-
-        this.setState({
-            movies :movie
-        })
-
-    }
-
-    handleToggleFav = (movie) => {
-        const { movies } = this.state;
-        const mid = movies.indexOf(movie);
-
-        movies[mid].fav = !movies[mid].fav;
-
-        this.setState({
-            movies
-        })
-    }
-
-    handleToggleCart = (movie) => {
-        const { movies } = this.state;
-        const mid = movies.indexOf(movie);
-
-        movies[mid].isInCart = !movies[mid].isInCart;
-
-        this.setState({
-            movies
-        })
-    }
+class MovieList extends Component {
+    
     render() {
         // const {title,plot,price,rating,stars,fav,isIncart,poster} = this.state.movies
-        const { movies } = this.state;
+        const { movies,addStars,decStars,toggleFav,toggleCart } = this.props;
+        console.log(this.props)
 
         return (
             <>
-                {movies.map((movie) => ( <MovieCard movies={movie}
-                                                    addStars={this.handleIncStars}
-                                                    decStars={this.handleDecStars}
-                                                    toggleFav={this.handleToggleFav}
-                                                    toggleCart={this.handleToggleCart} 
-                                                    key ={movie.id}/>
 
-                ))}
+                {movies.map((movie) => <MovieCard movies={movie}
+                                                  addStars={addStars}
+                                                  decStars={decStars}
+                                                  toggleFav={toggleFav}
+                                                  toggleCart={toggleCart}
+                                                    key ={movie.id}/>)}
             </>
         )
     }
